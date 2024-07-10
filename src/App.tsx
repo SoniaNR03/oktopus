c4d3291760de63a5760dee74294e1ad553667544
sonia <sonianr03@gmail.com>
2024-07-10 20:45:14 +0200

    feat: General distribution of account and fileManager columns

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
+import Accounts from "./Components/Accounts";
+import FileManager from "./Components/FileManager";
 function App() {
   return (
     <>
-      <p>Hola mundo</p>
+      <Accounts />
+      <FileManager />
     </>
   );
 }
